adbd246262a59fd72fe1a6b8a9f7918258775221
Phil Baum <phil@phib.io>
2018-09-23 13:05:38 +0200

    Db Client

---
+++ yasl/lib/util/db_client.dart
@@ -0,0 +1,88 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:yasl/model/list_item.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper _instance = new DatabaseHelper.internal();
+
+  factory DatabaseHelper() => _instance;
+
+  final String tableItems = "listItemTable";
+  final String columnId = "id";
+  final String columnName = "name";
+  final String columnDateCreated = "dateCreated";
+
+  static Database _db;
+
+  Future<Database> get db async {
+    if (_db != null) {
+      return _db;
+    }
+    _db = await initDb();
+
+    return _db;
+  }
+
+  DatabaseHelper.internal();
+
+  initDb() async {
+    Directory documentDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentDirectory.path, "maindb.db");
+    var ourDb = await openDatabase(path, version: 1, onCreate: _onCreate);
+    return ourDb;
+  }
+
+  void _onCreate(Database db, int newVersion) async {
+    await db.execute(
+        "CREATE TABLE $tableItems($columnId INTEGER PRIMARY KEY, $columnName TEXT, $columnDateCreated TEXT)");
+  }
+
+  Future<int> saveItem(ListItem item) async {
+    var dbClient = await db;
+    int res = await dbClient.insert("$tableItems", item.toMap());
+    return res;
+  }
+
+  Future<List> getAllItems() async {
+    var dbClient = await db;
+    var result = await dbClient.rawQuery("SELECT * FROM $tableItems");
+
+    return result.toList();
+  }
+
+  Future<int> getCount() async {
+    var dbClient = await db;
+    return Sqflite.firstIntValue(
+        await dbClient.rawQuery("SELECT COUNT(*) FROM $tableItems"));
+  }
+
+  Future<ListItem> getItem(int id) async {
+    var dbClient = await db;
+
+    var result = await dbClient
+        .rawQuery("SELECT * FROM $tableItems WHERE $columnId = $id");
+    if (result.length == 0) return null;
+    return new ListItem.fromMap(result.first);
+  }
+
+  Future<int> deleteItem(int id) async {
+    var dbClient = await db;
+
+    return await dbClient
+        .delete(tableItems, where: "$columnId = ?", whereArgs: [id]);
+  }
+
+  Future<int> updateItem(ListItem user) async {
+    var dbClient = await db;
+    return await dbClient.update(tableItems, user.toMap(),
+        where: "$columnId = ?", whereArgs: [user.id]);
+  }
+
+  Future close() async {
+    var dbClient = await db;
+    return dbClient.close();
+  }
+}
