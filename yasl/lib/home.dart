dc8bc12d8ab35f538208f930c84b669095e9c3ba
Phil Baum <phil@phib.io>
2018-09-23 12:01:15 +0200

    Icons

--- yasl/lib/home.dart
+++ yasl/lib/home.dart
@@ -2,50 +2,49 @@ import 'package:flutter/material.dart';
 import './pages/shoppinglist.dart';
 import './pages/recepies.dart';
 
-
 class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
-
-
   TabController _tabController;
 
   @override
-    void initState() {
-      // TODO: implement initState
-      super.initState();
-      _tabController = new TabController(vsync: this, initialIndex: 0, length: 2);
-    }
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tabController = new TabController(vsync: this, initialIndex: 0, length: 2);
+  }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-       appBar: new AppBar(
-         title: new Text("YASL"),
-         elevation: 0.7,
-         bottom: new TabBar(
-           controller: _tabController,
-           indicatorColor: Colors.teal[700],
-           tabs: <Widget> [
-             new Tab(text: "Shopping list",),
-             new Tab(text: "Recepies")
-           ]
-         ),
-       ),
-       body: new TabBarView(
-         controller: _tabController,
-         children: <Widget> [
-           new Shoppinglist(),
-           new Recepies()
-         ]
-       ),
-       floatingActionButton: new FloatingActionButton(
-         backgroundColor: Theme.of(context).accentColor,
-         child: new Icon(Icons.add),
-         onPressed: () => print("add"),
-       ),
+      appBar: new AppBar(
+        title: new Text("YASL"),
+        //elevation: 0.7,
+        bottom: new TabBar(
+            controller: _tabController,
+            indicatorColor: Colors.teal[700],
+            tabs: <Widget>[
+              new Tab(
+                text: "Shopping list",
+              ),
+              new Tab(text: "Recepies")
+            ]),
+        actions: <Widget>[
+          new Icon(Icons.search),
+          new Padding(padding: const EdgeInsets.symmetric(horizontal: 5.0)),
+          new Icon(Icons.more_vert)
+        ],
+      ),
+      body: new TabBarView(
+          controller: _tabController,
+          children: <Widget>[new Shoppinglist(), new Recepies()]),
+      floatingActionButton: new FloatingActionButton(
+        backgroundColor: Theme.of(context).accentColor,
+        child: new Icon(Icons.add),
+        onPressed: () => Navigator.pushNamed(context,"Recepies"),
+      ),
     );
   }
 }
