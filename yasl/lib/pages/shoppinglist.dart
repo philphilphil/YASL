9823cfa284c25288b357ed5383e9c6824231af91
Phil Baum <phil@phib.io>
2018-09-23 14:18:32 +0200

    Displaying items, not really working yet

--- yasl/lib/pages/shoppinglist.dart
+++ yasl/lib/pages/shoppinglist.dart
@@ -1,25 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:yasl/model/list_item.dart';
+import 'package:yasl/util/db_client.dart';
 
-class Shoppinglist extends StatelessWidget {
+class Shoppinglist extends StatefulWidget {
+  _ShoppinglistState createState() => _ShoppinglistState();
+}
+
+class _ShoppinglistState extends State<Shoppinglist> {
+  var db = new DatabaseHelper();
+  final List<ListItem> _itemList = <ListItem>[];
+
+  @override
+  void initState() {
+    super.initState();
+
+    _readItemsToList();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ListView(
-        children: <Widget>[
-          ListTile(
-            leading: Icon(Icons.radio_button_unchecked),
-            title: Text('Milch'),
-          ),
-          ListTile(
-            leading: Icon(Icons.radio_button_unchecked),
-            title: Text('Schokolade'),
-          ),
-          ListTile(
-            leading: Icon(Icons.check_circle),
-            title: Text('Pizza'),
-          ),
-        ],
+    return Scaffold(
+      body: new Column(
+        children: <Widget> [
+          new Flexible(
+            child: new ListView.builder(
+              padding: new EdgeInsets.all(8.0),
+              reverse: false,
+              itemCount: _itemList.length,
+              itemBuilder: (_, int index) {
+                return Card(
+                  color: Colors.green,
+                  child: new ListTile(
+                    title: _itemList[index])
+                );
+              },
+            ),)
+        ]
       ),
+
     );
+  }
+
+  _readItemsToList() async {
+    List items = await db.getAllItems();
+    items.forEach((item) {
+      _itemList.add(ListItem.map(item));
+      print("item");
+    });
   }
 }
