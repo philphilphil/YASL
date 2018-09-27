3d7e42438819e2605156fe317abe530eb90696c1
Phil Baum <phil@phib.io>
2018-09-27 23:31:19 +0200

    Building own list tile because longtap

--- yasl/lib/util/ListItemCard.dart
+++ yasl/lib/util/ListItemCard.dart
@@ -9,22 +9,26 @@ class ListItemCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new CheckboxListTile(
-      secondary: new Container(
+    return new ListTile(
+      leading: new Container(
         height: 30.0,
         width: 3.0,
         color: Colors.green,
         margin: const EdgeInsets.only(left: 1.0, right: 1.0),
       ),
       title: new Text(name),
-      value: done,
-       isThreeLine: false,
-       subtitle: Text("1 x"),
-      onChanged: (bool value) {
+      trailing: new Checkbox(value: done,),
+      isThreeLine: false,
+      // subtitle: Text("1 x"),
+      onTap: () {
         // setState(() {
         print("tap");
         // });
       },
+      onLongPress: () {
+        print("long tap");
+      },
+      contentPadding: EdgeInsets.symmetric(horizontal: 1.0, vertical: 0.0),
     );
   }
 }
