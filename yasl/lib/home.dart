30914e16b5830a87d8e845d7acd61b30e5523ffc
Phil Baum <phil@phib.io>
2018-09-27 21:54:38 +0200

    Moved app button back again. for now :)

--- yasl/lib/home.dart
+++ yasl/lib/home.dart
@@ -11,12 +11,54 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   TabController _tabController;
   var db = new DatabaseHelper();
-
+  final TextEditingController _textEditingController =
+      new TextEditingController();
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     _tabController = new TabController(vsync: this, initialIndex: 0, length: 2);
+  }
+
+  void _handleSubmit(String text) async {
+    _textEditingController.clear();
+    ListItem item = new ListItem(text, DateTime.now().toIso8601String());
+    int savedId = await db.saveItem(item);
+    print("item saved with id $savedId");
+    // _reloadList();
+  }
+
+  void _showAddWidget() {
+    var alert = new AlertDialog(
+        content: new Row(
+          children: <Widget>[
+            new Expanded(
+              child: new TextField(
+                controller: _textEditingController,
+                autofocus: true,
+                decoration: new InputDecoration(
+                    labelText: "Item",
+                    hintText: "Name of the item",
+                    icon: new Icon(Icons.note_add)),
+              ),
+            )
+          ],
+        ),
+        actions: <Widget>[
+          new FlatButton(
+              onPressed: () {
+                _handleSubmit(_textEditingController.text);
+              },
+              child: Text("Save")),
+          new FlatButton(
+              onPressed: () => Navigator.pop(context), child: Text("Cancel"))
+        ]);
+
+    showDialog(
+        context: context,
+        builder: (_) {
+          return alert;
+        });
   }
 
   @override
@@ -46,7 +88,7 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
       floatingActionButton: new FloatingActionButton(
         backgroundColor: Theme.of(context).accentColor,
         child: new Icon(Icons.add),
-        onPressed: null, // _showAddWidget,
+        onPressed: _showAddWidget,
       ),
     );
   }
