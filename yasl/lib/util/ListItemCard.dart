df54c92db89281fde72c7095567d438c11473e25
Phil Baum <phil@phib.io>
2018-09-28 18:38:30 +0200

    Make ListItemCard a stateful widget

--- yasl/lib/util/ListItemCard.dart
+++ yasl/lib/util/ListItemCard.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
-class ListItemCard extends StatelessWidget {
+class ListItemCard extends StatefulWidget {
   String name;
   String category;
   bool done;
+  _ListItemCardState createState() => _ListItemCardState();
 
   ListItemCard(this.name, this.done, this.category);
+}
+
+class _ListItemCardState extends State<ListItemCard> {
 
   @override
   Widget build(BuildContext context) {
@@ -16,8 +20,10 @@ class ListItemCard extends StatelessWidget {
         color: Colors.green,
         margin: const EdgeInsets.only(left: 1.0, right: 1.0),
       ),
-      title: new Text(name),
-      trailing: new Checkbox(value: done,),
+      title: new Text(widget.name),
+      trailing: new Checkbox(
+        value: widget.done,
+      ),
       isThreeLine: false,
       // subtitle: Text("1 x"),
       onTap: () {
@@ -26,9 +32,40 @@ class ListItemCard extends StatelessWidget {
         // });
       },
       onLongPress: () {
-        print("long tap");
+        _showAddWidget();
       },
       contentPadding: EdgeInsets.symmetric(horizontal: 1.0, vertical: 0.0),
     );
+  }
+
+  void _showAddWidget() {
+    var alert = new AlertDialog(
+        content: new Row(
+          children: <Widget>[
+            new Expanded(
+              child: new TextField(
+                //      controller: _textEditingController,
+                autofocus: true,
+                decoration: new InputDecoration(
+                    labelText: "Item",
+                    hintText: "Name of the item",
+                    icon: new Icon(Icons.note_add)),
+              ),
+            )
+          ],
+        ),
+        actions: <Widget>[
+          new FlatButton(
+              onPressed: () {
+                //_handleSubmit(_textEditingController.text);
+              },
+              child: Text("Save"))
+        ]);
+
+    showDialog(
+        context: context,
+        builder: (_) {
+          return alert;
+        });
   }
 }
